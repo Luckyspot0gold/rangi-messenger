4051f78ad7c0f6df0fceba35471f1a4a3bc0635b
Justin McCrea <mammerjam303@gmail.com>
2025-09-24 14:21:31 -0600

    Create starter.ts

---
+++ Demo/sample/starter.ts
@@ -0,0 +1,8 @@
+// Simple proof-of-concept to build in next 60 minutes
+class QuickStartMessenger {
+  async demoHarmonicAlert() {
+    console.log('🎵 Playing 432Hz baseline...');
+    // Add basic Web Audio code here
+    return { success: true, frequency: 432 };
+  }
+}
